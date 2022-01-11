405dd69c2d5ad3210e082348f91c2a23ce96e00c
unknown <rcolon85@gmail.com>
2022-01-10 23:49:54 -0500

    api module added to ruby
    Added the api into ruby as a module
    added module api on line one
    added module v1 #it has to be capitalized

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,11 @@
+module Api
+    module V1
+        class BooksController < ApplicationController
+
+        def index
+          books = Book.all
+          render json: books
+        end    
+     end
+   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
   resources :books
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+namespace :api  do
+  namespace :v1 do
+    resources :books, defaults: {format: :json}
+
+  end
+
+end
+
 
   # Defines the root path route ("/")
   # root "articles#index"
 end
+  # Define your application routes
+  # per the DSL in https://guides.rubyonrails.org/routing.html
